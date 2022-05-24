95e0e47cf8f9a2dbb3cc0c7f4163546075ed01a6
Todd Albert <todd@bocacode.com>
2022-05-24 16:33:45 -0400

    added photo to hero

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import Hero from './src/components/Hero';
 import Counter from './src/components/Counter';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.heading}>Open up App.js to start working on your app!</Text>
-      <Button title='Press me please' />
+      <Hero />
       <Counter />
       <StatusBar style="auto" />
     </View>
@@ -14,10 +14,6 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
-  heading: {
-    fontSize: 38,
-    color: '#fff'
-  },
   container: {
     flex: 1,
     backgroundColor: '#ffaa00',
--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,20 +1,27 @@
+import { useState } from 'react';
 import { View, Text, Button, StyleSheet } from 'react-native';
 
 export default function Counter() {
+  const [count, setCount] = useState(0);
   return (
     <View style={styles.counterContainer}>
-      <Text style={styles.counterText}>You clicked the button 0 times.</Text>
-      <Button title="Click me" />
+      <Text style={styles.counterText}>You clicked the button {count} times.</Text>
+      <Button title="+" onPress={() => setCount(count + 1)} />
+      <Button title="Reset" onPress={() => setCount(0)} />
+      <Button title="-" onPress={() => setCount(count - 1)} />
     </View>
   )
 }
 
 const styles = StyleSheet.create({
   counterContainer: {
-    width: '80vw',
+    width: '80%',
     margin: 14,
     padding: 14,
     backgroundColor: 'beige',
+    borderColor: 'navy',
+    borderWidth: 2,
+    borderRadius: 16,
   },
   counterText: {
     fontSize: 30,
